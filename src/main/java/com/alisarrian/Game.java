d9bd647071f92842a5c7ae441559b0e1c3835ef3
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-14 16:09:34 +0100

    Throw when field is occupied impl

--- src/main/java/com/alisarrian/Game.java
+++ src/main/java/com/alisarrian/Game.java
@@ -1,12 +1,24 @@
 package com.alisarrian;
 
 public class Game {
+    private String[][] board = {
+            {"", "", ""},
+            {"", "", ""},
+            {"", "", ""}
+    };
+
     void play(int x, int y) {
         if (x < 1 || x > 3) {
-            throw new RuntimeException("X is outside board");
+            throw new RuntimeException("X is outside board.");
         }
         if (y < 1 || y > 3) {
-            throw new RuntimeException("Y is outside board");
+            throw new RuntimeException("Y is outside board.");
+        }
+
+        if (board[x - 1][y - 1] != "") {
+            throw new RuntimeException("This field is occupied.");
+        } else {
+            board[x - 1][y - 1] = "occupied";
         }
     }
 }
