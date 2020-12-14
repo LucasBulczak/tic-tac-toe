8f358d83f0988ca3b1953535a3f4398c2206571b
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-14 17:47:59 +0100

    Turn queue refactor

--- src/test/java/com/alisarrian/TurnQueueTest.java
+++ src/test/java/com/alisarrian/TurnQueueTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
@@ -14,34 +15,27 @@ public class TurnQueueTest {
 
     private static final Player X = new Player("X");
     private static final Player Y = new Player("Y");
-    private Collection<Player> playersQueue;
 
     @BeforeMethod
     public void setUp() {
-        playersQueue = new ArrayList<>();
-        playersQueue.add(X);
-        playersQueue.add(Y);
-        turnQueue = new TurnQueue(playersQueue);
+        turnQueue = new TurnQueue(List.of(X, Y));
     }
 
     @Test
     void shouldReturnXAsActivePlayer() {
-        Player actual = turnQueue.getActivePlayer();
-        assertEquals(actual, X);
+        assertEquals(turnQueue.getActivePlayer(), X);
     }
 
     @Test
-    void shouldReturnYAsASecondPlayer() {
+    void shouldReturnYAsASecondActivePlayer() {
         turnQueue.next();
-        Player actual = turnQueue.getActivePlayer();
-        assertEquals(actual, Y);
+        assertEquals(turnQueue.getActivePlayer(), Y);
     }
 
     @Test
     void shouldRollEntireQueue() {
         turnQueue.next();
         turnQueue.next();
-        Player actual = turnQueue.getActivePlayer();
-        assertEquals(actual, X);
+        assertEquals(turnQueue.getActivePlayer(), X);
     }
 }
