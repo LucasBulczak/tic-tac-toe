97f536200aeaa1a24a5f30e956479b34cddb6a9d
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-14 16:45:19 +0100

    First player test

---
+++ src/test/java/com/alisarrian/PlayerTest.java
@@ -0,0 +1,13 @@
+package com.alisarrian;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.fail;
+
+public class PlayerTest {
+
+    @Test
+    void shouldReturnXAsDefaultFirstPlayer() {
+        fail("Not implemented yet!");
+    }
+}
