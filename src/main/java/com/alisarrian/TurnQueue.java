95346b67fcfc9ea88fdce8de01581886ef7e183e
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-14 17:14:33 +0100

    Queue first impl

--- src/main/java/com/alisarrian/TurnQueue.java
+++ src/main/java/com/alisarrian/TurnQueue.java
@@ -1,13 +1,27 @@
 package com.alisarrian;
 
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class TurnQueue {
-    public TurnQueue(Collection<Player> players) {
+    private Player activePlayer;
+    private final Queue<Player> playerQueue;
+    private final Collection<Player> playerList;
 
+    public TurnQueue(Collection<Player> players) {
+        playerList = players;
+        playerQueue = new LinkedList<>();
+        playerQueue.addAll(playerList);
+
+        next();
     }
 
     Player getActivePlayer() {
-        return null;
+        return activePlayer;
+    }
+
+    void next() {
+        activePlayer = playerQueue.poll();
     }
 }
