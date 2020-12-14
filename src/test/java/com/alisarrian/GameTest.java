4dd6a133fc08493beb69dbc1f492ea872e848418
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-14 15:57:08 +0100

    X coordinate out of the board test

--- src/test/java/com/alisarrian/GameTest.java
+++ src/test/java/com/alisarrian/GameTest.java
@@ -1,11 +1,19 @@
 package com.alisarrian;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class GameTest {
 
+    private Game game;
+
     @BeforeMethod
     public void setUp() {
-        final var game = new Game();
+        game = new Game();
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    void shouldThrowRuntimeExceptionWhenFieldOutsideOfBoard() {
+        game.play(4, 0);
     }
 }
