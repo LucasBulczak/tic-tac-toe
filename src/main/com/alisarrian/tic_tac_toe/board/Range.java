a9a95041a3799b3f639192386ee8655dad8c54e4
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-15 23:10:52 +0100

    New class Range

---
+++ src/main/com/alisarrian/tic_tac_toe/board/Range.java
@@ -0,0 +1,28 @@
+package com.alisarrian.tic_tac_toe.board;
+
+class Range {
+
+    private final int lowerBound;
+    private final int upperBound;
+
+    public Range(int upperBound) {
+        this(0, upperBound);
+    }
+
+    private Range(int lowerBound, int upperBound) {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+    }
+
+    public int getLowerBound() {
+        return lowerBound;
+    }
+
+    public int getUpperBound() {
+        return upperBound;
+    }
+
+    public boolean in(int value) {
+        return value >= lowerBound && value < upperBound;
+    }
+}
