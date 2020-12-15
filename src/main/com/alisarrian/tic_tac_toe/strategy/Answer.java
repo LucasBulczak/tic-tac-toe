9d138525c990e0e8e53ae2477a1a5c60a12c2c11
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-15 23:32:18 +0100

    Simple check for draw strategy impl & tests

--- src/main/com/alisarrian/tic_tac_toe/strategy/Answer.java
+++ src/main/com/alisarrian/tic_tac_toe/strategy/Answer.java
@@ -1,4 +1,6 @@
 package com.alisarrian.tic_tac_toe.strategy;
 
 public enum Answer {
+    NO_WINNER,
+    DRAW,
 }
--- src/main/com/alisarrian/tic_tac_toe/strategy/DrawStrategy.java
+++ src/main/com/alisarrian/tic_tac_toe/strategy/DrawStrategy.java
@@ -2,9 +2,15 @@ package com.alisarrian.tic_tac_toe.strategy;
 
 import com.alisarrian.tic_tac_toe.board.Field;
 
+import java.util.Arrays;
+
 public class DrawStrategy implements CheckStrategy {
     @Override
-    public Answer matches(Field playerSign, Field[][] board) {
-        return null;
+    public Answer matches(Field playerSign, Field[][] grid) {
+        return noAllFieldsTaken(grid) ? Answer.NO_WINNER : Answer.DRAW;
+    }
+
+    private boolean noAllFieldsTaken(Field[][] grid) {
+        return Arrays.stream(grid).flatMap(Arrays::stream).anyMatch(field -> field == Field.EMPTY);
     }
 }
