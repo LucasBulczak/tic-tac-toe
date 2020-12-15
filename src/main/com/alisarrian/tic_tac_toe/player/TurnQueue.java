20833065b3b6088e2a9ebd666ecc62671e5073ee
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-15 23:23:54 +0100

    Basic implementation of Player's queue

--- src/main/com/alisarrian/tic_tac_toe/player/TurnQueue.java
+++ src/main/com/alisarrian/tic_tac_toe/player/TurnQueue.java
@@ -1,4 +1,33 @@
 package com.alisarrian.tic_tac_toe.player;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TurnQueue {
+    private Player activePlayer;
+    private final Queue<Player> playerQueue;
+    private final Collection<Player> playerList;
+
+    public TurnQueue(Collection<Player> players) {
+        playerList = players;
+        playerQueue = new LinkedList<>();
+        init();
+        next();
+    }
+
+    public Player getActivePlayer() {
+        return activePlayer;
+    }
+
+    public void next() {
+        if (playerQueue.isEmpty()) {
+            init();
+        }
+        activePlayer = playerQueue.poll();
+    }
+
+    private void init() {
+        playerQueue.addAll(playerList);
+    }
 }
--- src/test/com/alisarrian/tic_tac_toe/player/TurnQueueTest.java
+++ src/test/com/alisarrian/tic_tac_toe/player/TurnQueueTest.java
@@ -1,7 +1,39 @@
 package com.alisarrian.tic_tac_toe.player;
 
+import com.alisarrian.tic_tac_toe.board.Field;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 import static org.testng.Assert.*;
 
 public class TurnQueueTest {
+    private static final Player X = new HumanPlayer(null, Field.X);
+    private static final Player O = new HumanPlayer(null, Field.O);
 
+    private TurnQueue turnQueue;
+
+    @BeforeMethod
+    public void setUp() {
+        turnQueue = new TurnQueue(List.of(X, O));
+    }
+
+    @Test
+    void shouldReturnXAsActivePlayer() {
+        assertEquals(turnQueue.getActivePlayer(), X);
+    }
+
+    @Test
+    void shouldReturnYAsASecondActivePlayer() {
+        turnQueue.next();
+        assertEquals(turnQueue.getActivePlayer(), O);
+    }
+
+    @Test
+    void shouldRollEntireQueue() {
+        turnQueue.next();
+        turnQueue.next();
+        assertEquals(turnQueue.getActivePlayer(), X);
+    }
 }
