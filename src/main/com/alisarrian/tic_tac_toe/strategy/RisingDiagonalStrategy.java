d32cd5fa089c03a2c624f1d9bc9b9ad474797a52
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-15 23:41:24 +0100

    New classes for checking rising diagonal

---
+++ src/main/com/alisarrian/tic_tac_toe/strategy/RisingDiagonalStrategy.java
@@ -0,0 +1,10 @@
+package com.alisarrian.tic_tac_toe.strategy;
+
+import com.alisarrian.tic_tac_toe.board.Field;
+
+public class RisingDiagonalStrategy implements CheckStrategy{
+    @Override
+    public Answer matches(Field playerSign, Field[][] board) {
+        return null;
+    }
+}
