f88557e6b1c247af3013dccd1f844c48554ca2aa
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-15 23:56:19 +0100

    Main game loop

--- src/main/game/TicTacToeGame.java
+++ src/main/game/TicTacToeGame.java
@@ -1,17 +1,28 @@
 package game;
 
 import com.alisarrian.tic_tac_toe.board.Board;
+import com.alisarrian.tic_tac_toe.player.Player;
 import com.alisarrian.tic_tac_toe.player.TurnQueue;
 import com.alisarrian.tic_tac_toe.ui.UI;
 
 public class TicTacToeGame {
     private final Board board;
-    private UI ui;
+    private final UI ui;
     private final TurnQueue queue;
+    private final Player activePlayer;
 
     public TicTacToeGame(UI ui, TurnQueue queue, int boardSize) {
         this.ui = ui;
         this.queue = queue;
         board = new Board(boardSize);
+        activePlayer = queue.getActivePlayer();
+    }
+
+    Player play() {
+        boolean noWinners = false;
+        do {
+
+        } while (!noWinners);
+        return activePlayer;
     }
 }
