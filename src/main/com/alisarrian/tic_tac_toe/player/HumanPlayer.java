4f3e901d05fc6edf4f1a74c2131b73263ba8b503
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-15 23:19:01 +0100

    New class HumanPlayer

---
+++ src/main/com/alisarrian/tic_tac_toe/player/HumanPlayer.java
@@ -0,0 +1,22 @@
+package com.alisarrian.tic_tac_toe.player;
+
+import com.alisarrian.tic_tac_toe.board.Field;
+import com.alisarrian.tic_tac_toe.board.Point;
+import com.alisarrian.tic_tac_toe.board.Range;
+
+public class HumanPlayer implements Player {
+    @Override
+    public Point getPoint(Range range) {
+        return null;
+    }
+
+    @Override
+    public Field getSign() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
