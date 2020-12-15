69d4b3ec9c6bb303d49ec4438016fa060e952f57
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-15 23:39:08 +0100

    VerticalStrategy impl & tests

--- src/main/com/alisarrian/tic_tac_toe/strategy/VerticalStrategy.java
+++ src/main/com/alisarrian/tic_tac_toe/strategy/VerticalStrategy.java
@@ -4,7 +4,14 @@ import com.alisarrian.tic_tac_toe.board.Field;
 
 public class VerticalStrategy implements CheckStrategy {
     @Override
-    public Answer matches(Field playerSign, Field[][] board) {
-        return null;
+    public Answer matches(Field playerSign, Field[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            int matches = 0;
+            for (Field[] fields : grid) {
+                if (fields[i] == playerSign) matches++;
+            }
+            if (matches == grid.length) return Answer.WINNER;
+        }
+        return Answer.NO_WINNER;
     }
 }
