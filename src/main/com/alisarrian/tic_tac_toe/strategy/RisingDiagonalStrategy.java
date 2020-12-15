6bfb97fb74f5589f04c36e3aaf351aa59084a313
Lucas Bulczak <lucas.bulczak@gmail.com>
2020-12-15 23:42:24 +0100

    Checking rising diagonal

--- src/main/com/alisarrian/tic_tac_toe/strategy/RisingDiagonalStrategy.java
+++ src/main/com/alisarrian/tic_tac_toe/strategy/RisingDiagonalStrategy.java
@@ -4,7 +4,11 @@ import com.alisarrian.tic_tac_toe.board.Field;
 
 public class RisingDiagonalStrategy implements CheckStrategy{
     @Override
-    public Answer matches(Field playerSign, Field[][] board) {
-        return null;
+    public Answer matches(Field playerSign, Field[][] grid) {
+        int matches = 0;
+        for (int i = (grid.length - 1), j = 0; i >= 0; i--, j++) {
+            if (grid[i][j] == playerSign) matches++;
+        }
+        return (matches == grid.length) ? Answer.WINNER : Answer.NO_WINNER;
     }
 }
